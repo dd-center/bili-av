ed40e3a28cb9e1520fc2da32fc98f87c2692ef9a
Il Harper <afanyiyu@hotmail.com>
2020-03-23 17:36:32 +0800

    feat: 🎸 BackEnd Convert Route Complete

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const Koa = require('koa')
+const Router = require('koa-router')
+const router = new Router()
+const axios = require('axios').default
 const { Nuxt, Builder } = require('nuxt')
 
 const app = new Koa()
@@ -30,6 +33,47 @@ async function start() {
       nuxt.render(ctx.req, ctx.res)
     }
   })
+
+  router.get('/api/convert', async (ctx) => {
+    if ('av' in ctx.request.query) {
+      const result = await axios
+        .get('http://www.bilibili.com/video/' + ctx.request.query.av, {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+          }
+        })
+        .catch(() => {
+          ctx.response.status = 500
+          ctx.response.body = ''
+        })
+      if (!result) return
+      ctx.response.status = 200
+      ctx.response.body = result.data.match(
+        /"videoData":{"bvid":"(\S*)","aid":/
+      )[1]
+    } else if ('bv' in ctx.request.query) {
+      const result = await axios
+        .get('http://www.bilibili.com/video/' + ctx.request.query.bv, {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+          }
+        })
+        .catch(() => {
+          ctx.response.status = 500
+          ctx.response.body = ''
+        })
+      if (!result) return
+      ctx.response.status = 200
+      ctx.response.body = result.data.match(/"aid":(\S*),"bvid":"/)[1]
+    } else {
+      ctx.response.status = 404
+      ctx.response.body = ''
+    }
+  })
+
+  app.use(router.routes()).use(router.allowedMethods())
 
   app.listen(port, host)
 }
