6ecdd95237dceff7c75e0cfb77e28d9a74ecdd90
Il Harper <afanyiyu@hotmail.com>
2020-03-23 16:52:38 +0800

    feat: 🎸 Nuxt Config Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,10 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    server: {
+      port: 2230
+    },
+    title: 'BiliBili AV/BV Utils',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
