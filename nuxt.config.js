ef725f70adf88d606bad108197d56bb7ab2565f1
Il Harper <afanyiyu@hotmail.com>
2020-03-24 15:41:57 +0800

    feat: 🎸 Website Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: ['element-ui/lib/theme-chalk/index.css'],
+  css: ['element-ui/lib/theme-chalk/index.css', '@/assets/main.css'],
   /*
    ** Plugins to load before mounting the App
    */
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,41 @@
 <template>
+  <scrollbar
+    style="height: 100vh; width: 100vw;"
+    wrap-style="display: flex; flex-direction: column; align-items: center;"
+  >
+    <div class="container" style="margin: 24px;">
+      <div class="container">
+        <img src="@/assets/logo.png" style="width: 100px;" />
+        <h2>AV/BV互转小工具</h2>
+        <section>
+          <el-input
+            v-model="input"
+            style="float: left;"
+            placeholder="在此输入av或bv号"
+            @keyup.enter.native="convert"
+          ></el-input>
+        </section>
+        <section>
+          <el-button type="plain" @click="convert">转换</el-button>
+          <el-button type="plain" @click="openWatch">观看</el-button>
+        </section>
+        <div style="margin: 10px;">
+          <a href="https://github.com/dd-center/bili-av" target="_blank">
+            <img
+              src="https://img.shields.io/github/stars/dd-center/bili-av?color=brightgreen&style=flat-square"
+            />
+          </a>
+        </div>
+        <p>AV/BV互转小工具，可以实现相互转换。</p>
+        <p>输入av+数字或纯数字不影响查询结果。</p>
+        <p>程序是在本地运行算法，可以无限次使用。</p>
+        <p>如果结果为空请查看输入是否有误。</p>
+        <p>遇到问题请私信@Il-Harper或加QQ1302744182或加群1053354128。</p>
+      </div>
+    </div>
+  </scrollbar>
+</template>
+<!--<template>
   <div style="float: left;">
     <section>
       <div>
@@ -24,19 +61,23 @@
       <p>结果：{{ result }}</p>
     </section>
   </div>
-</template>
+</template>-->
 
 <script>
+import SiderScrollbar from '@/components/scrollbar'
 import { enc, dec } from '@/utils/converter'
 export default {
+  components: {
+    scrollbar: SiderScrollbar
+  },
   data() {
     return {
-      input: '',
-      result: ''
+      input: ''
     }
   },
   methods: {
     convert() {
+      if (!this.input || this.input === '') return
       if (isNaN(Number(this.input))) {
         if (this.input.toLowerCase().startsWith('av')) {
           this.doAvConvert(this.input.toLowerCase())
@@ -53,7 +94,7 @@ export default {
       //   .catch(() => {})
       // if (!result) return
       // this.result = result.data
-      this.result = enc(value.replace('av', ''))
+      this.input = enc(value.replace('av', ''))
     },
     doBvConvert(value) {
       // const result = await this.$axios
@@ -61,7 +102,10 @@ export default {
       //   .catch(() => {})
       // if (!result) return
       // this.result = result.data
-      this.result = dec(value)
+      this.input = 'av' + dec(value)
+    },
+    openWatch() {
+      window.open('https://bilibili.com/video/' + this.input, '_blank')
     }
   },
   head() {
